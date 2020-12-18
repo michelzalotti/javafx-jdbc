6e34ad1f3fd9435b8bea35c26af507dceb960686
= <michelzalotti@gmail.com>
2020-12-18 20:49:13 -0300

    Inicio do projeto

---
+++ src/application/Program.java
@@ -0,0 +1,15 @@
+package application;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Program extends Application {
+
+    public void start(Stage mainStage) {
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
